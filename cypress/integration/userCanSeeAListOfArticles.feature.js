819539ebbd074e60174e5e41add12a2e0d49c3b1
flis_ann <ann.flismark@gmail.com>
2020-12-09 20:30:12 +0100

    Adding first test and fixture file

---
+++ cypress/integration/userCanSeeAListOfArticles.feature.js
@@ -0,0 +1,25 @@
+const { before } = require("cypress/types/lodash");
+
+describe("A user can see list of articles", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3001");
+  });
+
+  describe("if there are articles", () => {
+    before(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http//localhost:3000/api/articles",
+        response: "fixture:articles_data.json",
+      });
+    });
+
+    it("successfully", () => {
+      cy.get("[data-cy='article-index']").within(() => {
+        cy.contains('Cats are better than dogs!')
+        cy.contains('Have you noticed how smelly dogs are? Well that...')
+      })
+    });
+  });
+});
