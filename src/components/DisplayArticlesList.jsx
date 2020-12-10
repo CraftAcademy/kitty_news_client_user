eb59cd3a4baec38bf8dd3c1fd1be22de70e174a4
flis_ann <ann.flismark@gmail.com>
2020-12-10 10:22:43 +0100

    Adding code for DisplayArticlesList

--- src/components/DisplayArticlesList.jsx
+++ src/components/DisplayArticlesList.jsx
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from 'react'
 
 const DisplayArticlesList = () => {
-  us 
+  const [articles, setArticles] = useState([])
+  const getArticles = async () => {
+    let result = await getArticlesData()
+    setArticles(result.data.articles)
+  }
 
+useEffect(() => {
+  getArticles() 
+}, [])
+let articleIndex = articles.map(article => {
   return (
     <>
-      
+      <li>
+        {article.title}{article.lead}
+      </li>
+    </>
+       
+  )
+})
+  return (
+    <>
+      <ul key={item.id} data-cy="article-index">
+        {articleIndex}
+      </ul>
     </>
   )
 }
