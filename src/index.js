ae28ea5ac3871300eb95c7cb122f02d835de4e0d
psnoya <paulina.s.noya@gmail.com>
2020-12-10 16:23:50 +0100

    creating a .env file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 import "semantic-ui-css/semantic.min.css"
 
-axios.defaults.baseURL = "http://localhost:3001/api/"
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 ReactDOM.render(
     <App />,
