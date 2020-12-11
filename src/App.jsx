f032ac322ca4fce0c41729d47634fca8b837f5e1
psnoya <paulina.s.noya@gmail.com>
2020-12-11 13:20:54 +0100

    Added a route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import DisplayArticlesList from "./components/DisplayArticlesList";
+import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
+    <Switch>
+      <Route exact path ="/articles/:id" component={ShowArticle}/>
       <DisplayArticlesList />
+      </Switch>
     </>
   )
 }
