25ea1b640a05388361ec785a06c686e3b8194aaa
flis_ann <ann.flismark@gmail.com>
2020-12-12 20:39:47 +0100

    Creating sad path test for an article not showing correctly

--- cypress/integration/userCanReadASpecificArticle.feature.js
+++ cypress/integration/userCanReadASpecificArticle.feature.js
@@ -24,15 +24,42 @@ describe("User can", () => {
         cy.get("[data-cy='title']").should(
           "contain",
           "Cats are better than dogs!"
-				);
-				cy.get("[data-cy='lead']").should(
+        );
+        cy.get("[data-cy='lead']").should(
           "contain",
           "Have you noticed how smelly dogs are? Well that..."
-				);
-				cy.get("[data-cy='body']").should(
+        );
+        cy.get("[data-cy='body']").should(
           "contain",
           "is because they don't clean them self as good as cat does. Cats are always nice smelling and very well behaved. This is no lie, it is a simple truth-fact for sure!!"
-				);
+        );
+      });
+    });
+  });
+
+  context("not see an article if improper params are provided", () => {
+    beforeEach(() => {
+      cy.server();
+      cy.route({
+        method: "GET",
+        url: "http://localhost:3000/api/articles/1",
+        response: {
+          errors: "Something went wrong, this article was not found",
+        },
+        status: "404",
+      });
+      cy.visit("/");
+      cy.get("[data-cy='article-1']").click();
+    });
+    it("and is presented with an error  message ", () => {
+      cy.get("[data-cy='article-display']").within(() => {
+        cy.get("[data-cy='title']").should(
+          "not.exist",
+        );
+        cy.get("[data-cy='error-article']").should(
+          "contain",
+          "Something went wrong, this article was not found",
+        );
       });
     });
   });
