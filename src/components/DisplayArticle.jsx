f52120f73c70a3dc643bac5ba017956601e0c5ae
flis_ann <ann.flismark@gmail.com>
2020-12-12 19:49:39 +0100

    Fixing typo in fixture file and thus the test are passing and a user can now see a specific article

--- src/components/DisplayArticle.jsx
+++ src/components/DisplayArticle.jsx
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import getArticles from "../modules/getArticles";
+import { Container } from "semantic-ui-react";
 
 const DisplayArticle = () => {
-	const [article, setArticle] = useState({});
+  const [article, setArticle] = useState({});
   const { id } = useParams();
-	useEffect(() => {
-		const getSpecificArticle = async () => {
-      const response = await getArticles.show(id);
-      debugger
-			setArticle(response);
-		};
-		getSpecificArticle();
-	}, [id]);
+  const getSpecificArticle = async () => {
+    const response = await getArticles.show(id);
+    setArticle(response);
+  };
+  useEffect(() => {
+    getSpecificArticle();
+  }, [id]);
 
-	return (
-		<>
-			<div data-cy="article-display">
-				<h2 data-cy="title">{article.title}</h2>
-				<h3 data-cy="lead">{article.lead}</h3>
-				<p data-cy="body">{article.body}</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Container data-cy="article-display">
+        <h2 data-cy="title">{article.title}</h2>
+        <h3 data-cy="lead">{article.lead}</h3>
+        <p data-cy="body">{article.body}</p>
+      </Container>
+    </>
+  );
 };
 
 export default DisplayArticle;
