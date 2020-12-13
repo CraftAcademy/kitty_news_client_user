f9bc2ef3513bd956174fad45ca2bc5cb61c5622a
Sanne <sannelindholm@hotmail.com>
2020-12-13 10:39:19 +0100

    Creating a test for user to navigate from article back to news page

---
+++ cypress/integration/userCanNavigateToHeader.feature.js
@@ -0,0 +1,27 @@
+describe("A user can", () => {
+	beforeEach(() => {
+		cy.server();
+		cy.route({
+			method: "GET",
+			url: "http://localhost:3000/api/articles",
+			response: "fixture:articles_data.json",
+		});
+	});
+
+	context("successfully", () => {
+		beforeEach(() => {
+			cy.server();
+			cy.route({
+				method: "GET",
+				url: "http://localhost:3000/api/articles/1",
+				response: "fixture:single_article.json",
+			});
+			cy.visit("/");
+			cy.get("[data-cy='article-1']").click();
+		});
+
+		it("navigate from an article back to news page", () => {
+			cy.get;
+		});
+	});
+});
