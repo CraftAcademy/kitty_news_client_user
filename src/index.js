b61455574ca863b924e9b88ed548ae2643ef1a43
psnoya <paulina.s.noya@gmail.com>
2021-01-05 10:23:37 +0100

    Importing Provider. Adding configureStore

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 import "semantic-ui-css/semantic.min.css";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
+import "./index.css";
+import configureStore from "./state/store/configureStore";
+import { Provider } from "react-redux";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+const store = configureStore();
+window.store = store;
 
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
