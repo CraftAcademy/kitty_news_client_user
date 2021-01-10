1aa73e76457a4c1d5c17c4c180fce883599a913d
Kyungin Na <nakyungin93@gmail.com>
2021-01-10 16:43:54 +0100

    Formatting code Styling DisplayArticle.jsx with Grid

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -10,14 +10,12 @@ const ArticleCard = ({ article }) => {
       as={NavLink}
       to={`/articles/${article.id}`}
     >
-          
       <Item.Image
         size="small"
         wrapped
         ui={false}
         src="https://www.veterinarypracticenews.com/wp-content/uploads/2019/08/bigstock-Little-Striped-Cute-Kitten-Sit-244080397-300x200.jpg"
-        />
-
+      />
       <Item.Content>
         <Item.Description>{article.title}</Item.Description>
         <Item.Meta>{article.lead}</Item.Meta>
