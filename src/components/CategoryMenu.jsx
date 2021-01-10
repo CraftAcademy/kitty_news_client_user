9f71b7fc7c691224be4f606f6f1f5f82b63bf1f9
Kyungin Na <nakyungin93@gmail.com>
2021-01-10 19:35:23 +0100

    Adding header image Styling CategoryMenu Adding bg color

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
+import { NavLink, Link } from "react-router-dom";
+import { Menu, Segment } from "semantic-ui-react";
 import { getArticles } from "../modules/getArticles";
 import { useTranslation } from "react-i18next";
 
@@ -10,53 +10,63 @@ const CategoryMenu = () => {
   const dispatch = useDispatch();
 
   return (
-    <Menu>
-      <Menu.Item
-        name="global_politics"
-        data-cy="category-global-politics"
-        onClick={() => getArticles.index_by_category(1, dispatch)}
-        as={Link}
-        to="/"
-      >
-        {t("global_politics")}
-      </Menu.Item>
-      <Menu.Item
-        name="sports"
-        data-cy="category-sports"
-        onClick={() => getArticles.index_by_category(2, dispatch)}
-        as={Link}
-        to="/"
-      >
-        {t("sports")}
-      </Menu.Item>
-      <Menu.Item
-        name="self_care"
-        data-cy="category-self-care"
-        onClick={() => getArticles.index_by_category(3, dispatch)}
-        as={Link}
-        to="/"
-      >
-        {t("self_care")}
-      </Menu.Item>
-      <Menu.Item
-        name="news"
-        data-cy="category-news"
-        onClick={() => getArticles.index_by_category(4, dispatch)}
-        as={Link}
-        to="/"
-      >
-        {t("news")}
-      </Menu.Item>
-      <Menu.Item
-        name="culture"
-        data-cy="category-culture"
-        onClick={() => getArticles.index_by_category(5, dispatch)}
-        as={Link}
-        to="/"
-      >
-        {t("culture")}
-      </Menu.Item>
-    </Menu>
+    <Segment attached="bottom" inverted style={{ marginTop: "-0.05em"}}>
+      <Menu inverted borderless>
+        <Menu.Item
+          data-cy="header-news"
+          as={NavLink}
+          to="/"
+          onClick={() => getArticles.index(dispatch)}
+        >
+          {t("home_link")}
+        </Menu.Item>
+        <Menu.Item
+          name="global_politics"
+          data-cy="category-global-politics"
+          onClick={() => getArticles.index_by_category(1, dispatch)}
+          as={Link}
+          to="/"
+        >
+          {t("global_politics")}
+        </Menu.Item>
+        <Menu.Item
+          name="sports"
+          data-cy="category-sports"
+          onClick={() => getArticles.index_by_category(2, dispatch)}
+          as={Link}
+          to="/"
+        >
+          {t("sports")}
+        </Menu.Item>
+        <Menu.Item
+          name="self_care"
+          data-cy="category-self-care"
+          onClick={() => getArticles.index_by_category(3, dispatch)}
+          as={Link}
+          to="/"
+        >
+          {t("self_care")}
+        </Menu.Item>
+        <Menu.Item
+          name="news"
+          data-cy="category-news"
+          onClick={() => getArticles.index_by_category(4, dispatch)}
+          as={Link}
+          to="/"
+        >
+          {t("news")}
+        </Menu.Item>
+        <Menu.Item
+          name="culture"
+          data-cy="category-culture"
+          onClick={() => getArticles.index_by_category(5, dispatch)}
+          as={Link}
+          to="/"
+        >
+          {t("culture")}
+        </Menu.Item>
+      </Menu>
+    </Segment>
   );
 };
 
