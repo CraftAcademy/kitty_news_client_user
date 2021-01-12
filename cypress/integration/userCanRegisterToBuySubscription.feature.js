8a3bd71224d64c2c5d69dd68235b97b6473588e6
Sanne <sannelindholm@hotmail.com>
2021-01-12 21:11:51 +0100

    Updating data cy and test, adding Modal with button and message. Imporint useSelector, useState and implementing it.

--- cypress/integration/userCanRegisterToBuySubscription.feature.js
+++ cypress/integration/userCanRegisterToBuySubscription.feature.js
@@ -19,17 +19,19 @@ describe('user can register to buy a subscription', () => {
     cy.visit("/");
   });
   it("successfully", () => {
+    cy.get("[data-cy='signup-button']").click()
     cy.get("[data-cy='input-email']").type("registered@user.com");
     cy.get("[data-cy='input-password']").type("password");
     cy.get("[data-cy='input-password-confirmation']").type("password");
     cy.get("[data-cy='submit-btn']").click();
-    cy.get("[data-cy='register-success-message']").should("contain", "Your registration was a success!");
+    cy.get("[data-cy='signup']").should("not.exist");
   });
   it("unsuccessfully", () => {
+    cy.get("[data-cy='signup-button']").click()
     cy.get("[data-cy='input-email']").type("registered@user.com");
     cy.get("[data-cy='input-password']").type("password");
     cy.get("[data-cy='input-password-confirmation']").type("wrong_password");
     cy.get("[data-cy='submit-btn']").click();
-    cy.get("[data-cy='register-error-message']").should("contain", "something went wrong!");
+    cy.get("[data-cy='register-error-message']").should("contain", "Something went wrong!");
   })
 });
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,53 +1,75 @@
-import React from "react";
-import { Segment, Form, Button, Icon } from "semantic-ui-react";
-import { useDispatch } from "react-redux";
+import React, { useState} from "react";
+import { Form, Button, Icon, Modal, Message } from "semantic-ui-react";
+import { useDispatch, useSelector } from "react-redux";
 import { signUp } from "../modules/Authentication";
 
-
 const RegistrationForm = () => {
+  const currentUser = useSelector((state) => state.currentUser);
+  const [open, setOpen] = useState(false);
   const dispatch = useDispatch();
 
   return (
-    <Segment placeholder>
-      <Form
-        className="loginForm"
-        data-cy="login-form"
-        onSubmit={(event) => signUp(event, dispatch)}
+    <>
+      <Modal
+        closeIcon
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={
+          <Button data-cy="signup-button" basic color="teal">
+            Sign up!
+          </Button>
+        }
       >
-        <Form.Input
-          icon="at"
-          type="text"
-          label="Email"
-          name="email"
-          data-cy="input-email"
-          placeholder="Email"
-          iconPosition="left"
-        />
-        <Form.Input
-          icon="key"
-          type="password"
-          label="Password"
-          name="password"
-          data-cy="input-password"
-          placeholder="Password"
-          iconPosition="left"
-        />
-        <Form.Input
-          icon="key"
-          type="password"
-          label="Password Confirmation"
-          name="password_confirmation"
-          data-cy='input-password-confirmation'
-          placeholder="Password Confirmation"
-          iconPosition="left"
-        />
+        <Modal.Header>Create an account!</Modal.Header>
+        <Modal.Content>
+          <Form
+            data-cy="signup-form"
+            onSubmit={(event) => signUp(event, dispatch)}
+          >
+            <Form.Input
+              icon="at"
+              type="text"
+              label="Email"
+              name="email"
+              data-cy="input-email"
+              placeholder="Email"
+              iconPosition="left"
+            />
+            <Form.Input
+              icon="key"
+              type="password"
+              label="Password"
+              name="password"
+              data-cy="input-password"
+              placeholder="Password"
+              iconPosition="left"
+            />
+            <Form.Input
+              icon="key"
+              type="password"
+              label="Password Confirmation"
+              name="password_confirmation"
+              data-cy="input-password-confirmation"
+              placeholder="Password Confirmation"
+              iconPosition="left"
+            />
 
-        <Button data-cy="submit-btn" icon labelPosition="left">
-          <Icon name="user"></Icon>
-          Submit
-        </Button>
-      </Form>
-    </Segment>
+            <Button data-cy="submit-btn" icon labelPosition="left">
+              <Icon name="user"></Icon>
+              Submit
+            </Button>
+          </Form>
+        </Modal.Content>
+        <Modal.Description>
+          {currentUser && (
+            <Message data-cy="register-error-message" negative>
+              Something went wrong!
+            </Message>
+          )}
+        </Modal.Description>
+      </Modal>
+    </>
   );
 };
 
