43c019c34aac4302a078164e2c148a2bbe6b43d0
Kyungin Na <nakyungin93@gmail.com>
2021-01-13 09:44:54 +0100

    Fixing mocked response in test

--- cypress/integration/userCanRegisterToBuySubscription.feature.js
+++ cypress/integration/userCanRegisterToBuySubscription.feature.js
@@ -23,11 +23,13 @@ describe("user can register to buy a subscription", () => {
     });
     it("with valid credentials", () => {
       cy.get("[data-cy='signup-button']").click();
-      cy.get("[data-cy='input-email']").type("registered@user.com");
-      cy.get("[data-cy='input-password']").type("password");
-      cy.get("[data-cy='input-password-confirmation']").type("password");
-      cy.get("[data-cy='submit-btn']").click();
-      cy.get("[data-cy='signup']").should("not.exist");
+      cy.get("[data-cy='signup-form']").within(() => {
+        cy.get("[data-cy='input-email']").type("registered@user.com");
+        cy.get("[data-cy='input-password']").type("password");
+        cy.get("[data-cy='input-password-confirmation']").type("password");
+        cy.get("[data-cy='submit-btn']").click();
+        cy.get("[data-cy='signup']").should("not.exist");
+      });
     });
   });
   describe("unsuccessfully", () => {
@@ -37,10 +39,8 @@ describe("user can register to buy a subscription", () => {
         url: "http://localhost:3000/api/auth",
         status: "401",
         response: {
-          data: {
-            errors: {
-              full_messages: ["Invalid login credentials. Please try again."],
-            },
+          errors: {
+            full_messages: ["Invalid login credentials. Please try again."],
           },
           success: false,
         },
@@ -49,10 +49,14 @@ describe("user can register to buy a subscription", () => {
     });
     it("with invalid credentials", () => {
       cy.get("[data-cy='signup-button']").click();
-      cy.get("[data-cy='input-email']").type("registered@user.com");
-      cy.get("[data-cy='input-password']").type("password");
-      cy.get("[data-cy='input-password-confirmation']").type("wrong_password");
-      cy.get("[data-cy='submit-btn']").click();
+      cy.get("[data-cy='signup-form']").within(() => {
+        cy.get("[data-cy='input-email']").type("registered@user.com");
+        cy.get("[data-cy='input-password']").type("password");
+        cy.get("[data-cy='input-password-confirmation']").type(
+          "wrong_password"
+        );
+        cy.get("[data-cy='submit-btn']").click();
+      });
       cy.get("[data-cy='register-error-message']").should(
         "contain",
         "Invalid login credentials. Please try again."
